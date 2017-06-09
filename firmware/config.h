35c5d9d6d8373381dd255fa27bc407e0ae4bb1fd
Bert Burgemeister <trebbu@googlemail.com>
2017-06-09 17:00:47 +0200

    CC14 is our official USB PID (issued by pid.codes)

--- firmware/config.h
+++ firmware/config.h
@@ -21,7 +21,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID 0x1209
-#define PRODUCT_ID 0x5050
+#define PRODUCT_ID 0xCC14
 /* (Thanks to http://pid.codes) */
 #define DEVICE_VER 0x0001
 #define MANUFACTURER trebb
